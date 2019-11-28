80a310bfb2c4c195713e53c561f54871a3abcc8f
Tobias Olsson <Olsson>
2019-11-28 09:13:38 +0100

    Added a js file

---
+++ js/ButtonClickEvent.js
@@ -0,0 +1,20 @@
+function Button1_ClickEvent(event){
+    ReverseString();
+    document.getElementById
+}
+
+function Button2_ClickEvent(){
+
+}
+
+function Button3_ClickEvent(){
+
+}
+
+function Button4_ClickEvent(){
+
+}
+
+function Button5_ClickEvent(){
+
+}
